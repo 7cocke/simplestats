dc0e0d523ffc806beb0001c404c97ff679b79684
Wil Cocke <cocke@math.wisc.edu>
2016-01-15 13:36:30 +0800

    Added assertions for the empty tuple and empty list.

--- stats.py
+++ stats.py
@@ -1,7 +1,11 @@
 def mean(vals):
     """Calculate the arithmetic mean of a list of numbers in vals."""
     assert type(vals) is list or type(vals) is tuple, 'Wrong input format.'
-    
+    if vals == []:
+    	return 0.0 
+    elif vals == (): 
+    	return 0.0 
+    	 
     total = sum(vals)
     length = len(vals)
     return total/length
@@ -10,8 +14,12 @@ def test_mean():
 	assert mean([2,4]) == 3.0
 test_mean()
 
-    
+def test_empty():
+	assert mean([]) == 0.0 and mean(()) == 0.0
+test_empty()
+
 #print(mean([2,4]))
 #print(mean('2,3'))
+
 
 
