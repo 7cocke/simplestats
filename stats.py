3effb35354102db89529abb09d08b2603521bf55
Wil Cocke <cocke@math.wisc.edu>
2016-01-15 13:38:30 +0800

    Tidied up case-by-case analysis for empty input.

--- stats.py
+++ stats.py
@@ -1,13 +1,11 @@
 def mean(vals):
     """Calculate the arithmetic mean of a list of numbers in vals."""
     assert type(vals) is list or type(vals) is tuple, 'Wrong input format.'
-    if vals == []:
-    	return 0.0 
-    elif vals == (): 
-    	return 0.0 
     	 
     total = sum(vals)
     length = len(vals)
+    if length == 0:
+    	return 0.0
     return total/length
 
 def test_mean():
